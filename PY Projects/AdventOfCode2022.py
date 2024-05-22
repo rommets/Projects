8d34cc51457c41d3e9b59535ed399442bcbe4c10
rommets <rometemadi@gmail.com>
2024-05-22 20:52:39 +0330

    init

--- PY Projects/AdventOfCode2022.py
+++ PY Projects/AdventOfCode2022.py
@@ -1,4 +1,9 @@
 file=open("fileAdventOfCode.txt") 
 data=file.read()
-print(data)    
-
+food=data.split('\n')
+elf=data.split('\n\n')
+sum=0
+for food in elf:
+    sum += int(food)
+max(int(sum))
+print(max)
