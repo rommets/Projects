cc1b6ac365619d487c91509c49e3674cd0a5c4a9
rommets <rometemadi@gmail.com>
2024-10-18 02:40:53 +0330

    fixing 3 starting 5

--- PY Projects/strings/AAS2.py
+++ PY Projects/strings/AAS2.py
@@ -1,4 +1,4 @@
-directory = "C:\\Users\\Lenovo-T570\\Projects\\PY Projects\\strings"
+directory = "C:\\Users\\Lenovo-T570\\Projects\\PY Projects\\strings\\files"
 from array import array
 
 
@@ -13,65 +13,48 @@ def count_words():
     for line in lines:
         khat = len(line.split())
         count = len(line)
-        print("1) In \"" + line + "\" we have "+ str(khat) + " and "+ str(count)+" words")
+        print("1) In \"" + line + "\" we have "+ str(khat) + " lines" + " and "+ str(count)+" words.")
 
 
 def remove_element():
     input_array = array('i')
-    lines = read_file("numbers.txt")
+    lines = read_file("repeated.txt")
     remove_number = int(lines.pop(0))
-    lines.pop(0)
-    # why is it removing 12 also
     for line in lines:
         input_array.append(int(line))
-
     output_array = array('i')
-
     for num in input_array:
         if num != remove_number:
             output_array.append(num)
     print('2)',output_array,end='\n')
 
-# for line in lines:
-#     int_array.append(int(line))
-# print(int_array)
-
-# print("2) Enter as many numbers as you please (If you're done, type q):")
-# while True:
-#     current = input()
-#     if current == "q":
-#         break
-#     else:
-#         int_array.append(int(current))
-
-# remove_number = int(input("Enter number to remove:"))
-
-# def Report_card():
-#     report_card=dict()
-#     flag=True
-#     lines=read_file("reportcard.txt")
-#     report_card.append(lines)
-#          scores=float(input("Enter student,s score: "))
-#          report_card[names] = scores
-#          if report_card== {}:
-#             print("Please enter at least 1 name")
-#             flag=True
-#     N=float(input("Now enter your chosen score: "))
-#     for names , scores in report_card.items():
-#          if N == scores:
-#              print(names.upper(),"received that score")
-#          if N != scores:
-#              print("-")
-#     print(end='\n')
+def Report_card():
+    report_card={}
+    lines=(read_file("reportcard.txt"))
+    selected_score=float(lines.pop(0))
+    lines.pop(0)
+    for i in lines:
+         name,grade=i.split(" ")
+         report_card[float(grade)]=name
+    print("3)",report_card[selected_score], "recieved that score.")
 
 def Index():
     flag=True
-    lines=read_file('numbersWOspace.txt')
-    print('4)')
+    lines=read_file('random.txt')
+    print('4) Here are the indexes: ')
     for i in lines:
       print(i,"'s index =",lines.index(i))
 
+# def reverse():
+#     lines=read_file("random.txt")
+#     input_array2=array('i')
+#     for i in lines:
+#         input_array2.append(int(i))
+#     print(input_array2[])
+
+
 count_words()
 remove_element()
-# Report_card()
+Report_card()
 Index()
+# reverse()
